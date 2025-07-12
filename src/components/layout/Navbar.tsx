2621f911c1ddf6d59008aa853310eeda2af11e5e
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-07-12 21:19:43 +0600

    Handel dark mode using shadcn and updated navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,43 +1,49 @@
 import { Link } from "react-router";
+import { ModeToggle } from "../mode-toggler";
 
 export default function Navbar() {
   return (
-    <nav className="max-w-7xl mx-auto h-16 flex items-center gap-3 px-5">
-      <div className="flex items-center">
-        <svg
-          id="logo-15"
-          width="100%"
-          height="32px"
-          viewBox="0 0 49 48"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M24.5 12.75C24.5 18.9632 19.4632 24 13.25 24H2V12.75C2 6.53679 7.03679 1.5 13.25 1.5C19.4632 1.5 24.5 6.53679 24.5 12.75Z"
-            className="ccustom"
-            fill="#22C55E"
-          />
-          <path
-            d="M24.5 35.25C24.5 29.0368 29.5368 24 35.75 24H47V35.25C47 41.4632 41.9632 46.5 35.75 46.5C29.5368 46.5 24.5 41.4632 24.5 35.25Z"
-            className="ccustom"
-            fill="#22C55E"
-          />
-          <path
-            d="M2 35.25C2 41.4632 7.03679 46.5 13.25 46.5H24.5V35.25C24.5 29.0368 19.4632 24 13.25 24C7.03679 24 2 29.0368 2 35.25Z"
-            className="ccustom"
-            fill="#22C55E"
-          />
-          <path
-            d="M47 12.75C47 6.53679 41.9632 1.5 35.75 1.5H24.5V12.75C24.5 18.9632 29.5368 24 35.75 24C41.9632 24 47 18.9632 47 12.75Z"
-            className="ccustom"
-            fill="#22C55E"
-          />
-        </svg>{" "}
-        <span className="font-bold ml-2 ">Task</span>Manager
-      </div>
+    <nav className="max-w-7xl mx-auto h-16 flex items-center justify-between gap-3 px-5">
+      <Link to="/">
+        <div className="flex items-center cursor-pointer">
+          <svg
+            id="logo-15"
+            width="100%"
+            height="32px"
+            viewBox="0 0 49 48"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M24.5 12.75C24.5 18.9632 19.4632 24 13.25 24H2V12.75C2 6.53679 7.03679 1.5 13.25 1.5C19.4632 1.5 24.5 6.53679 24.5 12.75Z"
+              className="ccustom"
+              fill="#22C55E"
+            />
+            <path
+              d="M24.5 35.25C24.5 29.0368 29.5368 24 35.75 24H47V35.25C47 41.4632 41.9632 46.5 35.75 46.5C29.5368 46.5 24.5 41.4632 24.5 35.25Z"
+              className="ccustom"
+              fill="#22C55E"
+            />
+            <path
+              d="M2 35.25C2 41.4632 7.03679 46.5 13.25 46.5H24.5V35.25C24.5 29.0368 19.4632 24 13.25 24C7.03679 24 2 29.0368 2 35.25Z"
+              className="ccustom"
+              fill="#22C55E"
+            />
+            <path
+              d="M47 12.75C47 6.53679 41.9632 1.5 35.75 1.5H24.5V12.75C24.5 18.9632 29.5368 24 35.75 24C41.9632 24 47 18.9632 47 12.75Z"
+              className="ccustom"
+              fill="#22C55E"
+            />
+          </svg>{" "}
+          <span className="font-bold ml-2 ">Task</span>Manager
+        </div>
+      </Link>
       <div className="flex items-center gap-1 font-semibold">
         <Link to="/users">User</Link>
         <Link to="/tasks">Task</Link>
+      </div>
+      <div className="ml-auto">
+        <ModeToggle />
       </div>
     </nav>
   );
