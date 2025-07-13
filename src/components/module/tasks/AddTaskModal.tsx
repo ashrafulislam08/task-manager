ce4cf5384c6dd7f86b0775ff61e3b60825feeb34
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-07-13 12:46:18 +0600

    created modal using shadcn ui

---
+++ src/components/module/tasks/AddTaskModal.tsx
@@ -0,0 +1,62 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+export function AddTaskModal() {
+  const form = useForm();
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Add Task</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Task</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input {...field} placeholder="Title" />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter className="mt-8">
+              <DialogClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DialogClose>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/pages/Task.tsx
+++ src/pages/Task.tsx
@@ -2,19 +2,21 @@ import { selectTasks } from "@/redux/features/todoSlice";
 import { useAppSelector } from "@/redux/hook";
 import Container from "@/components/ui/container";
 import TaskCard from "@/components/module/tasks/TaskCard";
+import { AddTaskModal } from "@/components/module/tasks/AddTaskModal";
 
 const Task = () => {
   const tasks = useAppSelector(selectTasks);
   console.log(tasks);
   return (
     <Container className="mt-20">
-      <div>
+      <div className="flex items-center justify-between my-5">
         <h1>Tasks</h1>
+        <AddTaskModal />
       </div>
 
       <div className="space-y-5 ">
-        {tasks.map((task) => (
-          <TaskCard task={task} />
+        {tasks.map((task, idx) => (
+          <TaskCard key={idx} task={task} />
         ))}
       </div>
     </Container>
